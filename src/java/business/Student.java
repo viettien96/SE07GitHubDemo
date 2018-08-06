56dbc1d3accd225a77e32de752fd4ab29ba76ae9
vy viet tien <vvtien9696@gmail.com>
2018-08-06 09:35:36 +0700

    lol

---
+++ src/java/business/Student.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+/**
+ *
+ * @author KIMANH
+ */
+public class Student {
+    private int studentId;
+    private String studentName;
+
+    public Student() {
+    }
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    
+    
+}
---
+++ src/java/controller/StudentController.java
@@ -0,0 +1,17 @@
+
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/list")
+public class StudentController {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+                    throws IOException, ServletException{
+        
+    }
+}
